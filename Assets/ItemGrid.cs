c497e9ac466301541ddac039ba5d1e37f1ea0bf4
Redream <91314959+v-golyadkin@users.noreply.github.com>
2023-12-01 11:49:13 +0300

    Add item

--- Assets/ItemGrid.cs
+++ Assets/ItemGrid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,17 +6,54 @@ using UnityEngine;
 
 public class ItemGrid : MonoBehaviour
 {
+    InventoryItem[,] inventoryItemGrid;
+
     const float TileSizeWidth = 32f;
     const float TileSizeHeight = 32f;
+
+    [SerializeField] int gridSizeWidth;
+    [SerializeField] int gridSizeHeight;
 
     RectTransform rectTransform;
 
     Vector2 mousePositionOnTheGrid;
     Vector2Int tileGridPosition = new Vector2Int();
 
+    [SerializeField] GameObject inventoryItemPrefab;
+
+    private void Start()
+    {
+        Init(gridSizeWidth, gridSizeHeight);
+        GameObject itemTest = Instantiate(inventoryItemPrefab);
+        InventoryItem inventoryItemTest = itemTest.GetComponent<InventoryItem>();
+        PlaceItem(inventoryItemTest, 2, 2);
+    }
+
+    private void Init(int width, int height)
+    {
+        inventoryItemGrid = new InventoryItem[width, height];
+        Vector2 size = new Vector2();
+        size.x = TileSizeWidth * width;
+        size.y = TileSizeHeight * height;
+        rectTransform.sizeDelta = size;
+    }
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void PlaceItem(InventoryItem itemToPlace, int x, int y)
+    {
+        RectTransform itemRectTransform = itemToPlace.GetComponent<RectTransform>();
+        itemRectTransform.SetParent(transform);
+
+        inventoryItemGrid[x, y] = itemToPlace;
+
+        Vector2 positionOnGrid = new Vector2();
+        positionOnGrid.x = TileSizeWidth * x + TileSizeWidth / 2;
+        positionOnGrid.y = -(TileSizeHeight * y + TileSizeHeight / 2);
+        itemRectTransform.localPosition = positionOnGrid;
     }
 
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
