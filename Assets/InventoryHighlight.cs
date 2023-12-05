92f430eb647e58147d17cbdea4166dfba54a402f
Redream <91314959+v-golyadkin@users.noreply.github.com>
2023-12-05 23:30:22 +0300

    Add InventoryHighlight script

---
+++ Assets/InventoryHighlight.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHighlight : MonoBehaviour
+{
+    [SerializeField] RectTransform highliter;
+}
